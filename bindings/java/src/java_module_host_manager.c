0b4acc9758c0ca4633a000f7fe8c1ad9cdb632b6
andrew-buckley <andrewbuckley@outlook.com>
2016-06-06 23:06:27 -0700

    adds requirements for java module host manager.

--- bindings/java/src/java_module_host_manager.c
+++ bindings/java/src/java_module_host_manager.c
@@ -23,20 +23,26 @@ JAVA_MODULE_HOST_MANAGER_HANDLE JavaModuleHostManager_Create()
 
 	if (instance == NULL)
 	{
+		/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_002: [ The function shall allocate memory for the JAVA_MODULE_HOST_MANAGER_HANDLE if the global instance is NULL. ]*/
 		result = (JAVA_MODULE_HOST_MANAGER_HANDLE_DATA*)malloc(sizeof(JAVA_MODULE_HOST_MANAGER_HANDLE_DATA));
 		if (result == NULL)
 		{
+			/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_003: [ The function shall return NULL if memory allocation fails. ]*/
 			LogError("Failed to allocate memory for a JAVA_MODULE_HOST_MANAGER_HANDLE");
 		}
 		else
 		{
+			/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_004: [ The function shall initialize a new LOCK_HANDLE and set the JAVA_MODULE_HOST_MANAGER_HANDLE structures LOCK_HANDLE member. ]*/
 			if ((result->lock = Lock_Init()) == NULL)
 			{
+				/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_005: [ The function shall return NULL if lock initialization fails. ]*/
 				LogError("Lock_Init() failed.");
 				result = NULL;
 			}
 			else
 			{
+				/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_006: [ The function shall set the module_count member variable to 0. ]*/
+				/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_007: [ The function shall shall set the global JAVA_MODULE_HOST_MANAGER_HANDLE instance. ]*/
 				result->module_count = 0;
 				instance = result;
 			}
@@ -44,8 +50,11 @@ JAVA_MODULE_HOST_MANAGER_HANDLE JavaModuleHostManager_Create()
 	}
 	else
 	{
+		/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_001: [ The function shall return the global JAVA_MODULE_HOST_MANAGER_HANDLE instance if it is not NULL. ]*/
 		result = instance;
 	}
+
+	/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_008: [ The function shall return the global JAVA_MODULE_HOST_MANAGER_HANDLE instance. ]*/
 	return result;
 }
 
@@ -53,18 +62,22 @@ void JavaModuleHostManager_Destroy(JAVA_MODULE_HOST_MANAGER_HANDLE manager)
 {
 	if (manager == NULL)
 	{
+		/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_009: [ The function shall do nothing if handle is NULL. ]*/
 		LogError("JAVA_MODULE_HOST_MANAGER_HANDLE is NULL.");
 	}
 	else
 	{
 		if (manager->module_count == 0)
 		{
+			/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_011: [ The function shall deinitialize handle->lock. ]*/
 			if (Lock_Deinit(manager->lock) != LOCK_OK)
 			{
+				/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_012: [ The function shall do nothing if lock deinitialization fails. ]*/
 				LogError("Lock_Deinit() failed.");
 			}
 			else
 			{
+				/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_013: [ The function shall free the handle parameter. ]*/
 				free(manager);
 				manager = NULL;
 				instance = NULL;
@@ -72,6 +85,7 @@ void JavaModuleHostManager_Destroy(JAVA_MODULE_HOST_MANAGER_HANDLE manager)
 		}
 		else
 		{
+			/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_010: [ The function shall do nothing if handle->module_count is not 0. ]*/
 			LogInfo("Manager module count is not 0 (%i) and will not be destroyed.", manager->module_count);
 		}
 	}
@@ -89,7 +103,9 @@ JAVA_MODULE_HOST_MANAGER_RESULT JavaModuleHostManager_Remove(JAVA_MODULE_HOST_MA
 
 size_t JavaModuleHostManager_Size(JAVA_MODULE_HOST_MANAGER_HANDLE manager)
 {
-	return manager->module_count;
+	/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_021: [ If handle is NULL the function shall return -1. ]*/
+	/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_022: [ The function shall return handle->module_count. ]*/
+	return manager == NULL ? -1 : manager->module_count;
 }
 
 static JAVA_MODULE_HOST_MANAGER_RESULT internal_inc_dec(JAVA_MODULE_HOST_MANAGER_HANDLE manager, int addend)
@@ -98,26 +114,34 @@ static JAVA_MODULE_HOST_MANAGER_RESULT internal_inc_dec(JAVA_MODULE_HOST_MANAGER
 
 	if (manager == NULL)
 	{
+		/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_014: [ The function shall return MANAGER_ERROR if handle is NULL. ]*/
 		LogError("JAVA_MODULE_HOST_MANAGER_HANDLE is NULL.");
 		result = MANAGER_ERROR;
 	}
 	else
 	{
+		/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_015: [ The function shall acquire the handle parameters lock. ]*/
 		if (Lock(manager->lock) != LOCK_OK)
 		{
+			/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_016: [ If lock acquisition fails, this function shall return MANAGER_ERROR. ]*/
 			LogError("Failed to acquire lock.");
 			result = MANAGER_ERROR;
 		}
 		else
 		{
+			/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_017: [ The function shall increment or decrement handle->module_count. ]*/
 			manager->module_count += addend;
+
+			/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_018: [ The function shall release the handle parameters lock. ]*/
 			if (Unlock(manager->lock) != LOCK_OK)
 			{
+				/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_019: [ The function shall return MANAGER_ERROR if the function fails to release the lock. ]*/
 				LogError("Failed to release lock.");
 				result = MANAGER_ERROR;
 			}
 			else
 			{
+				/*Codes_SRS_JAVA_MODULE_HOST_MANAGER_14_020: [ The function shall return MANAGER_OK if the all succeeds. ]*/
 				result = MANAGER_OK;
 			}
 		}
